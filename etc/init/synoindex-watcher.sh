68a11dc7597db417fd1ed9a5ceb23302452ed971
Torben <torben.letorbi@gmail.com>
2019-08-04 23:54:45 +0200

    Rename command-names in init script

--- etc/init/synoindex-watcher.sh
+++ etc/init/synoindex-watcher.sh
@@ -1,22 +1,21 @@
 #!/bin/sh
-#S99synotify.sh
 
 # https://codesourcery.wordpress.com/2012/11/29/more-on-the-synology-nas-automatically-indexing-new-files/
 
 case "$1" in
   start|"")
     #start the monitoring daemon
-    synotify
+    synoindex-watcher
     ;;
   restart|reload|force-reload)
     echo "Error: argument '$1' not supported" >&2
     exit 3
     ;;
   stop)
-    kill `cat /var/run/synotify.pid`
+    kill `cat /var/run/synoindex-watcher.pid`
     ;;
   *)
-    echo "Usage: S99synotify.sh [start|stop]" >&2
+    echo "Usage: synoindex-watcher.sh [start|stop]" >&2
     exit 3
     ;;
 esac

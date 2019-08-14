a80512a6d21f97f0dd9a31d21651434b59feac71
Torben <torben.letorbi@gmail.com>
2019-08-14 15:29:38 +0200

    Add license header to scripts

--- etc/init/synoindexwatcher.sh
+++ etc/init/synoindexwatcher.sh
@@ -1,5 +1,23 @@
 #!/bin/sh
 
+# This file is part of Synoindex Watcher.
+#
+# Copyright (c) 2012-2018 Mark Houghton <https://codesourcery.wordpress.com>
+# Copyright (c) 2019 Torben Haase <https://pixelsvsbytes.com>
+#
+# Synoindex Watcher is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# Synoindex Watcher is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.You should have received a copy of the GNU General Public License
+# along with Synoindex Watcher.  If not, see <http:#www.gnu.org/licenses/>.
+#
+################################################################################
+
 case "$1" in
   start|"")
     mkdir -p /var/run/synoindexwatcher

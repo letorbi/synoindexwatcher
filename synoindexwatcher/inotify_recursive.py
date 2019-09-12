ceadf5321971ebf3158ad510be40eb08ee2e1e39
Torben <torben.letorbi@gmail.com>
2019-09-12 20:31:27 +0200

    Ensure that dirs outside the recursive-watch-tree are not watched anymore

--- synoindexwatcher/inotify_recursive.py
+++ synoindexwatcher/inotify_recursive.py
@@ -85,9 +85,14 @@ class INotify(inotify_simple.INotify):
                   elif event.mask & flags.MOVED_TO:
                       self.__last_moved_to = [str.encode(event.name), event.wd] # name, parent
               elif event.mask & flags.MOVE_SELF:
-                  self.__watch_info[event.wd]["name"] = self.__last_moved_to[0]
-                  self.__watch_info[event.wd]["parent"] = self.__last_moved_to[1]
-                  logging.debug("Updated info for watch %d: %s" % (event.wd, self.__watch_info[event.wd]))
+                  if self.__last_moved_to != None and self.__last_moved_to[1] in self.__watch_info:
+                    self.__watch_info[event.wd]["name"] = self.__last_moved_to[0]
+                    self.__watch_info[event.wd]["parent"] = self.__last_moved_to[1]
+                    logging.debug("Updated info for watch %d: %s" % (event.wd, self.__watch_info[event.wd]))
+                  else:
+                    logging.debug("Going to remove watch %d" % (event.wd))
+                    inotify_simple.INotify.rm_watch(self, event.wd)
+                  self.__last_moved_to = None
               elif event.mask & flags.IGNORED:
                   logging.debug("Queueing info for watch %d for removal" % (event.wd))
                   self.__watch_info_delete_queue.append(event.wd)

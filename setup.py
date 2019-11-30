7ecf7eec44159ed879669618290e657e3f2328ec
Torben <torben.letorbi@gmail.com>
2019-11-30 18:55:21 +0100

    Bump version to 0.8.0

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="synoindexwatcher",
-    version="0.7.3",
+    version="0.8.0",
     author="Torben Haase",
     author_email="torben@pixelsvsbytes.com",
     description="An automated media-index updater for Synology DiskStations based on inotify and synoindex.",
